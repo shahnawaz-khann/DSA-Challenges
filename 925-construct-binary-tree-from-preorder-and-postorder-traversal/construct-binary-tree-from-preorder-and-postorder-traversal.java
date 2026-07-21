098d817c37d8ed97dc283cdc52cfa067c9913e6d
Shahnawaz Khan <shah.khann418@gmail.com>
2026-07-21 21:02:18 +0530

    Time: 2 ms (7.56%) | Memory: 45.3 MB (26.32%) - LeetSync

---
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, Integer> map = new HashMap<>();
+    int i = 0;
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        for(int i=0; i<postorder.length; i++) {
+            map.put(postorder[i], i);
+        }
+        return buildTree(preorder, postorder, 0, postorder.length - 1);
+    }
+    private TreeNode buildTree(int[] preorder, int[] postorder, int left, int right) {
+        if(left > right) {
+            return null;
+        }
+        int rootval = preorder[i++];
+        TreeNode root = new TreeNode(rootval);
+        if(left == right) {
+            return root;
+        }
+        int leftroot = preorder[i];
+        int mid = map.get(leftroot);
+
+        
+        root.left = buildTree(preorder, postorder, left, mid);
+        root.right = buildTree(preorder, postorder, mid + 1, right - 1);
+
+        return root;
+    }
+}
