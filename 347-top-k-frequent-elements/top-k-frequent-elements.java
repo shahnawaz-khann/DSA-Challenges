da4bb73ceaeb5a4ffc65620d0ce710801d88d800
Shahnawaz Khan <shah.khann418@gmail.com>
2026-07-24 15:15:40 +0530

    Time: 15 ms (55.01%) | Memory: 47.5 MB (66.61%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -3,18 +3,15 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
 
         PriorityQueue<Integer> pq = new PriorityQueue<>(
-            (a, b) -> map.get(a) - map.get(b)
+            (a, b) -> map.get(b) - map.get(a)
         );
 
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        for(int key : map.keySet()) {
+        for(int key : map.keySet()) {       
             pq.add(key);
-            if(pq.size() > k) {
-                pq.poll();
-            }
         }
         int[] ans = new int[k];
 
