b774f6ac87a8d543121fc9862e2f6450264cd767
Shahnawaz Khan <shah.khann418@gmail.com>
2026-08-23 01:32:20 +0530

    Time: 5 ms (62.07%) | Memory: 44.5 MB (66.77%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,13 +2,17 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
 
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
+        int[] freq = new int[26];
 
         for(int i=0; i<s.length(); i++) {
-            map1.put(s.charAt(i), map1.getOrDefault(s.charAt(i), 0) + 1);
-            map2.put(t.charAt(i), map2.getOrDefault(t.charAt(i), 0) + 1);
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
         }
-        return map1.equals(map2);
+        for(int x : freq) {
+            if(x != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
