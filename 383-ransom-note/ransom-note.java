3eb1853cd459f7435f812c838dcb8f0ac5826dc7
Shahnawaz Khan <shah.khann418@gmail.com>
2026-08-23 01:54:14 +0530

    Time: 16 ms (19.30%) | Memory: 47.2 MB (13.04%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -3,18 +3,21 @@ class Solution {
         if(magazine.length() < ransomNote.length()) {
             return false;
         }
-        int[] freq = new int[26];
+        
+        Map<Character, Integer> map = new HashMap<>();
 
         for(int i=0; i<magazine.length(); i++) {
-            if(i < ransomNote.length()) {
-                freq[ransomNote.charAt(i) - 'a']--;
-            }
-            freq[magazine.charAt(i) - 'a']++;
+            char ch = magazine.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        for(int x : freq) {
-            if(x <= -1) {
-                return false;
-            }
+
+        for(int i=0; i<ransomNote.length(); i++) {
+            char ch = ransomNote.charAt(i);
+            
+            if(!map.containsKey(ch)) return false;
+
+            map.put(ch, map.get(ch) - 1);
+            if(map.get(ch) == 0) map.remove(ch);
         }
         return true;
     }
