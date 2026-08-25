cb926bb90425e627ed753c6f74fc9b25ae1481f2
Shahnawaz Khan <shah.khann418@gmail.com>
2026-08-25 23:32:03 +0530

    Time: 4 ms (99.06%) | Memory: 47.9 MB (29.21%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean validPalindrome(String s) {
         int i=0; int j = s.length() - 1;
-        int count = 0;
         while(i < j) {
             if(s.charAt(i) != s.charAt(j)) {
                 return isPalindrom(s, i+1, j) || isPalindrom(s, i, j-1);
