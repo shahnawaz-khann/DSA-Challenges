06ca33ac19390f8fd6bfba25dadec3c78a675726
Shahnawaz Khan <shah.khann418@gmail.com>
2026-08-25 20:17:21 +0530

    Time: 2 ms (99.31%) | Memory: 44.5 MB (54.26%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,17 +1,18 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
-        StringBuilder sb = new StringBuilder();
-        for(char c : s.toCharArray()) {
-            if(Character.isLetterOrDigit(c)) {
-                sb.append(c);
-            }
-        }
-        int i = 0; int j = sb.length() - 1;
+        int i = 0;
+        int j = s.length() - 1;
+
         while(i < j) {
-            if(sb.charAt(i) != sb.charAt(j)) {
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
                 return false;
-            } 
+            }
             i++; j--;
         }
         return true;
