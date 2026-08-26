dd72f2366aece3168a7928eabe5415736447f63d
Shahnawaz Khan <shah.khann418@gmail.com>
2026-08-26 21:22:32 +0530

    Time: 14 ms (15.70%) | Memory: 47.1 MB (26.19%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,26 +1,23 @@
-class Solution {
-    public String reverseWords(String s) {
-        String[] arr = s.split(" ");
-
-        for(int i=0; i<arr.length; i++) {
-            arr[i] = reverse(arr[i]);
+    class Solution {
+        public String reverseWords(String s) {
+            String[] str = s.split("\\s+");
+            
+            for(int i=0; i<str.length; i++) {
+                str[i] = reverse(str[i].toCharArray());
+            }
+            return String.join(" ", str);
         }
-        return String.join(" ", arr);
+        private String reverse(char[] arr) {
+            int i = 0;
+            int j = arr.length - 1;
 
-    }
-    private String reverse(String s) {
-        int i = 0;
-        int j = s.length() - 1;
-        char[] arr = s.toCharArray();
+            while(i < j) {
+                char temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
 
-        while(i < j) {
-            char temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-
-            i++;
-            j--;
+                i++; j--;
+            }
+            return new String(arr);
         }
-        return new String(arr);
     }
-}
