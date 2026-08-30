1a9d75f25df0b25946e589d44f4109fc409ae8c7
Shahnawaz Khan <shah.khann418@gmail.com>
2026-08-30 23:03:29 +0530

    Time: 29 ms (25.58%) | Memory: 53.4 MB (5.65%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,16 +1,34 @@
 class Solution {
-    int count = 0;
     public int countSubstrings(String s) {
-        for(int i=0; i<s.length(); i++) {
-            expand(s, i, i);
-            expand(s, i, i+1);
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (isPalindrome(s, i, j, dp) == 1) {
+                    count++;
+                }
+            }
         }
         return count;
+
     }
-    private void expand(String s, int left, int right) {
-        while(left >= 0 && right <= s.length()-1 && s.charAt(left) == s.charAt(right)) {
-            count++;
-            left--; right++;
+    private int isPalindrome(String s, int i, int j, int[][] dp) {
+        if(i >= j) {
+            return 1;
         }
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if(s.charAt(i) == s.charAt(j)) {
+            dp[i][j] = isPalindrome(s, i+1, j-1, dp);
+            return dp[i][j];
+        }
+        return dp[i][j] = 0;
     }
 }
